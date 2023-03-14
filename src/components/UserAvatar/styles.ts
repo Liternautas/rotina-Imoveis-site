4ff8b442d19bbaef6c7751eb95e98d9210097c88
EzequielPires <ezequiel.pires082000@gmail.com>
2023-03-14 20:26:53 -0300

    Commit

--- src/components/UserAvatar/styles.ts
+++ src/components/UserAvatar/styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
+import { styled, TableContainer } from "@mui/material";
 
-export const Container = styled.div`
+export const Container = styled(TableContainer)`
 
 `
