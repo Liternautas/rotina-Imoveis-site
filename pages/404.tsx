26cb64fb1f7c72f4ce7ec5bd155ad47cecd3159f
EzequielPires <ezequiel.pires082000@gmail.com>
2023-03-31 10:28:55 -0300

    Commit

--- pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import { GetServerSideProps } from "next"
-
-export default function Error404Page() {
-    return (
-        <div>Erro 404</div>
-    )
-}
--- pages/500.tsx
+++
@@ -1,7 +0,0 @@
-import { GetServerSideProps } from "next"
-
-export default function Error500Page() {
-    return (
-        <div>Erro 500</div>
-    )
-}
