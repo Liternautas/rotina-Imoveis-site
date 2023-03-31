f381c4d3f79bcf427876c4cc8067b9ad832815f4
EzequielPires <ezequiel.pires082000@gmail.com>
2023-03-30 23:20:00 -0300

    Commit

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -4,7 +4,7 @@ export const getImageUrl = (path: string) => {
     if (!path) {
         return '/no-image.png';
     }
-    return path?.startsWith('storage') ? `${process.env.NEXT_PUBLIC_BASE_URL}${path}` : path;
+    return path?.startsWith('storage') ? `https://rotina-imoveis.herokuapp.com/${path}` : path;
 }
 
 export const normalize = (value: string) => slugify(value, {lower: true});
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import {parseCookies} from "nookies";
 export function getApi(ctx?: any) {
     const { 'imob.token': token } = parseCookies(ctx);
     const api = axios.create({
-        baseURL: process.env.NEXT_PUBLIC_URL_DEFAULT
+        baseURL: 'https://rotina-imoveis.herokuapp.com/'
     })
     if (token) api.defaults.headers['Authorization'] = `Bearer ${token}`;
     return api;
