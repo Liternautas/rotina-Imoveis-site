3b9153705a659c4ba85fa425680684cf83f49a2a
EzequielPires <ezequiel.pires082000@gmail.com>
2023-03-30 23:11:18 -0300

    Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { api } from '@/src/services/api';
 import { Home } from '@/src/ui/screens/Site/Home';
 import { GetServerSideProps } from 'next';
 
@@ -8,13 +9,12 @@ export default function HomePage({properties}) {
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  //const { results, count } = await api.get('properties').then(res => res.data);
-  /* console.log(results); */
+  const { results, count } = await api.get('properties').then(res => res.data);
 
   return {
       props: {
-          properties: /* results ?? [] */ [],
-          total: /* count ?? */ 0
+          properties: results ?? [],
+          total: count ?? 0
       }
   }
 }
