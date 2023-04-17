131ea73e14d93f55ae83b683908b66479e6ce8e6
EzequielPires <ezequiel.pires082000@gmail.com>
2023-04-17 18:55:51 -0300

    Commit

--- src/ui/screens/Site/Contact/styles.ts
+++ src/ui/screens/Site/Contact/styles.ts
@@ -24,22 +24,30 @@ export const Links = styled(Box)`
     display: flex;
     flex-direction: column;
     gap: .5rem;
-
+    svg {
+        color: ${({theme}) => theme.palette.primary.main};
+        transition: .3s;
+    }
     a {
         text-decoration: none;
         color: #FFF4D3;
         transition: .2s;
         :hover {
             color: ${({theme}) => theme.palette.primary.main};
+            svg {
+                color: #FFF4D3 !important;
+            }
         }
     }
     button {
         background: #FFF4D3;
+        transition: .3s;
         :hover {
             background: ${({theme}) => theme.palette.primary.main};
+            svg {
+                color: #FFF4D3 !important;
+            }
         }
     }
-    svg {
-        color: ${({theme}) => theme.palette.primary.main};
-    }
+    
 `
